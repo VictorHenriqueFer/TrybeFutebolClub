1b4e202a0d8296fc16399d98fd9d374da0c461dd
Victor Henrique <victorhenriquefernandes2@gmail.com>
2024-02-21 11:22:27 -0300

    corrigindo

--- app/backend/src/tests/users.test.ts
+++ app/backend/src/tests/users.test.ts
@@ -15,12 +15,26 @@ const { expect } = chai;
 
 describe('Users Test', function() {
     it('Testa se ao passar um email invalido retorna um erro', async function() {
-        sinon.stub(SequelizeUsers, 'findOne').resolves(null)
-        const { status, body } = await chai.request(app).get('/login');
-        expect(status).to.equal(400);
-        expect(body).to.deep.equal({ message: 'Invalid email' });
-    }
-    );
+        const { status, body } =  await chai.request(app).post('/login').send({ email: 'invalidemail@email.com', password: '123456' });
+        expect(status).to.equal(401);
+        expect(body).to.deep.equal({ message: 'Invalid email or password' });
+    })
+    it('Testa se ao passar um email valido e senha invalida retorna um erro', async function() {
+        const { status, body } =  await chai.request(app).post('/login')
+        .send({ email: 'user@user.com',
+        password: '123456' });
+        expect(status).to.equal(401);
+        expect(body).to.deep.equal({ message: 'Invalid email or password' });
+    })
+    it.only('Testa se ao passar um email valido e senha valida retorna um token', async function() {
+        const { status, body } =  await chai.request(app).post('/login')
+        .send({ email: "user@user.com",
+        password: "secret_user"
+        });
+        console.log(body)
+        expect(status).to.equal(500);
+        expect(body).to.have.property('token');
+    });
     afterEach(sinon.restore);
 });
 
